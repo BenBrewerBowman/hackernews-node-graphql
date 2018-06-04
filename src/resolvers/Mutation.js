77ea0aa6d52ebac38a155cda979668471ff8ae93
benbrewerbowman <benbrewerbowman@gmail.com>
2018-06-04 15:23:52 -0600

    Voting for link added

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,9 +1,10 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { APP_SECRET, getUserId } = require('../utils')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET, getUserId } = require('../utils');
+
 
 function post(parent, args, context, info) {
-  const userId = getUserId(context)
+  const userId = getUserId(context);
   return context.db.mutation.createLink(
     {
       data: {
@@ -16,13 +17,14 @@ function post(parent, args, context, info) {
   )
 }
 
+
 async function signup(parent, args, context, info) {
-  const password = await bcrypt.hash(args.password, 10)
+  const password = await bcrypt.hash(args.password, 10);
   const user = await context.db.mutation.createUser({
     data: { ...args, password },
   }, `{ id }`)
 
-  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
   return {
     token,
@@ -30,15 +32,16 @@ async function signup(parent, args, context, info) {
   }
 }
 
+
 async function login(parent, args, context, info) {
-  const user = await context.db.query.user({ where: { email: args.email } }, `{ id password }`)
+  const user = await context.db.query.user({ where: { email: args.email } }, `{ id password }`);
   if (!user) {
-    throw new Error('No such user found')
+    throw new Error('No such user found');
   }
 
-  const valid = await bcrypt.compare(args.password, user.password)
+  const valid = await bcrypt.compare(args.password, user.password);
   if (!valid) {
-    throw new Error('Invalid password')
+    throw new Error('Invalid password');
   }
 
   return {
@@ -47,8 +50,34 @@ async function login(parent, args, context, info) {
   }
 }
 
+
+async function vote(parent, args, context, info) {
+
+  const userId = getUserId(context);
+
+  const linkExists = await context.db.exists.Vote({
+    user: { id: userId },
+    link: { id: args.linkId }
+  });
+  if (linkExists) {
+    throw new Error('Already voted for link: ${args.linkId');
+  }
+
+  return context.db.mutation.createVote(
+    {
+      data: {
+        user: { connect: { id: userId } },
+        link: { connect: { id: args.linkId } }
+      }
+    },
+    info,
+  );
+}
+
+
 module.exports = {
   post,
   signup,
-  login
+  login,
+  vote
 }
